75e03bb9ecc6c77b0328b6cfa18669891290b2ee
sat3x18 <nikolozpc@gmail.com>
2025-09-15 23:21:12 +0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC<HeaderProps> = ({ categories }) => {
             {/* Logo */}
             <Link to="/" className="flex items-center">
               <div className="text-2xl font-bold text-navy-900 tracking-tight font-serif">
-                NIKOLAS MAISON
+                TBILISI WEAR
               </div>
             </Link>
 
